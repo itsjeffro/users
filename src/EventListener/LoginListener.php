73ae63f582da89f5242f9e819a03189e89456f2c
Jeff Nielsen <jeff.nielsen@4mation.com.au>
2019-04-17 21:21:28 +1000

    Handle redirects based on user role

--- src/EventListener/LoginListener.php
+++ src/EventListener/LoginListener.php
@@ -2,24 +2,56 @@
 
 namespace App\EventListener;
 
+use App\Entity\Employee;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 
 class LoginListener
 {
+    /**
+     * @var RouterInterface
+     */
+    public $router;
+
     /**
      * @var EntityManagerInterface
      */
     public $entityManager;
 
     /**
+     * @var TokenStorageInterface
+     */
+    public $tokenStorage;
+
+    /**
+     * @var EventDispatcherInterface
+     */
+    public $dispatcher;
+
+    /**
      * LoginListener constructor.
      *
+     * @param TokenStorageInterface $tokenStorage
+     * @param RouterInterface $router
      * @param EntityManagerInterface $entityManager
+     * @param EventDispatcherInterface $dispatcher
      */
-    public function __construct(EntityManagerInterface $entityManager)
-    {
+    public function __construct(
+        TokenStorageInterface $tokenStorage,
+        RouterInterface $router,
+        EntityManagerInterface $entityManager,
+        EventDispatcherInterface $dispatcher
+    ) {
+        $this->router = $router;
         $this->entityManager = $entityManager;
+        $this->tokenStorage = $tokenStorage;
+        $this->dispatcher = $dispatcher;
     }
 
     /**
@@ -36,5 +68,26 @@ class LoginListener
 
         $this->entityManager->persist($user);
         $this->entityManager->flush();
+
+        $this->dispatcher->addListener(KernelEvents::RESPONSE, [$this, 'onKernelResponse']);
+    }
+
+    /**
+     * Handle redirect for user.
+     *
+     * @param FilterResponseEvent $event
+     */
+    public function onKernelResponse(FilterResponseEvent $event)
+    {
+        $user = $this->tokenStorage->getToken()->getUser();
+        $url = $this->router->generate('contractor');
+
+        if (in_array(Employee::ROLE_EMPLOYEE, $user->getRoles())) {
+            $url = $this->router->generate('employee');
+        }
+
+        $response = new RedirectResponse($url);
+
+        return $event->setResponse($response);
     }
 }
